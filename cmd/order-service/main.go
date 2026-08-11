e619c0c7a117b19fccebee1c214c208e1c1c2a09
Golf <nfetiskin@gmail.com>
2026-08-10 23:35:28 -0700

    Added order main

---
+++ cmd/order-service/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	orderv1 "ecommerce/gen/order/v1"
+	productv1 "ecommerce/gen/product/v1"
+	userv1 "ecommerce/gen/user/v1"
+	"ecommerce/internal/order"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	userConn, err := grpc.NewClient("127.0.0.1:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer userConn.Close()
+
+	productConn, err := grpc.NewClient("127.0.0.1:50052", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer productConn.Close()
+
+	userClient := userv1.NewUserServiceClient(userConn)
+	productClient := productv1.NewProductServiceClient(productConn)
+
+	lis, err := net.Listen("tcp", ":50053")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	grpcServer := grpc.NewServer()
+	store := order.NewStore()
+	orderv1.RegisterOrderServiceServer(grpcServer, order.NewServer(store, userClient, productClient))
+	reflection.Register(grpcServer)
+
+	log.Println("50053")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
+}

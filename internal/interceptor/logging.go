5b48ee44904d23ac46f6fea3db83277cc385bdf4
Golf <nfetiskin@gmail.com>
2026-08-11 21:40:24 -0700

    used any

--- internal/interceptor/logging.go
+++ internal/interceptor/logging.go
@@ -8,7 +8,7 @@ import (
 	"google.golang.org/grpc"
 )
 
-func LoggingUnaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+func LoggingUnaryInterceptor(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
 	start := time.Now()
 
 	resp, err := handler(ctx, req)
